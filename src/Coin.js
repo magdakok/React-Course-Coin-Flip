a4c03a753a4a37defc7cdbd4bc2423e41b205a0b
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-06 12:00:49 +0100

    add base animation to the coin

--- src/Coin.js
+++ src/Coin.js
@@ -1,13 +1,20 @@
 import React, {Component} from "react";
 import front from "./front.png";
 import back from "./back.png";
+import "./Coin.css";
 
 
 class Coin extends Component {
     render(){
         return (
             <div className="Coin">
-                { this.props.side==="front" ? <img src={front}/> : <img src={back}/>}
+                <div className="Coin-side Coin-side-front">
+                    { this.props.side==="front" ? <img src={front}/> : <img src={back}/>}
+                </div>
+
+                <div className="Coin-side Coin-side-back">
+                { this.props.side==="back" ? <img src={front}/> : <img src={back}/>}
+                </div>
             </div>
         )
     }
