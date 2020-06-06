7ffc04ea9655f2e5655dd4b0ad07020e4d044fdd
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-06 11:12:29 +0100

    add Counter component

---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Counter extends Component {
+    render(){
+        return (
+            <div className="Counter">
+                <p>Counter here</p>
+            </div>
+        )
+    }
+}
+
+export default Counter;
--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
 import Coin from "./Coin";
+import Counter from "./Counter";
 
 class Game extends Component {
     render(){
         return (
             <div className="Game">
                 <Coin side="back" />
+                <Counter />
             </div>
         )
     }
